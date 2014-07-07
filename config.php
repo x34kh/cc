2f626b1833a4a2f000565bb85f0cec4d9f43474b
Alexander S <alexander@nixsupport.net>
2014-07-07 11:57:39 +0000

    minor edit

---
+++ config.php
@@ -0,0 +1,18 @@
+<?php
+//Levels of logging 
+// 0 = debug
+// 1 = info
+// 2 = error 
+$log_level	=	0;
+
+
+$log_folder	=	"./logs/";
+$general_log	=	$log_folder."general.log";
+
+//Database configuration
+$db_host	=	"localhost";
+$db_user	=	"commandcenter";
+$db_password	=	"dc5459#yT94I17q";
+
+
+?>
